093df543ecc24b6bad5a990338f3f2ba2b7b2690
alkshmir <shirakam.itsuk252@gmail.com>
2022-09-10 23:45:46 +0900

    d

--- abc268/d.py
+++ abc268/d.py
@@ -8,13 +8,14 @@ for _ in range(M):
     T.append(input())
 T = set(T)
 found = False
+
 for s in itertools.permutations(S):
     username = '_'.join(s)
-    if username not in T:
+    if 3<=len(username)<=16  and username not in T:
         found = True
         break
     nokori = 16 - len(username)
-    for j in range(nokori):
+    for j in range(nokori+1):
         for basho in itertools.combinations_with_replacement(range(N-1), j):
             # _ を増やす
             tmp_s = list(s)
@@ -23,11 +24,14 @@ for s in itertools.permutations(S):
             
             username = '_'.join(tmp_s)
            
-            if username not in T:
+            if 3<=len(username)<=16 and username not in T:
                 found = True
                 break
         if found:
             break
+
+    if found:
+        break
 
         
 if found:
