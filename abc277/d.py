f57a1cfdb6b97003f2dedd00db286c961b603659
alkshmir <shirakam.itsuk252@gmail.com>
2022-11-12 23:37:09 +0900

    delete groups dictionary

--- abc277/d.py
+++ abc277/d.py
@@ -4,30 +4,30 @@ A = sorted([int(s) for s in input().split()])
 
 # 数字->group
 group_mapping = dict()
-groups = defaultdict(list)
+#groups = defaultdict(list)
 # 重複度
 #overlap = defaultdict(int)
 group_sum = defaultdict(int)
 for i in range(N):
     if i == 0:
         group_mapping[A[0]] = A[0]
-        groups[A[0]].append(A[0])
+        #groups[A[0]].append(A[0])
     else:
         if A[i] - A[i-1] == 1 or A[i] == A[i-1]:
             #print(i, A[i], A[i-1])
             group_mapping[A[i]] = group_mapping[A[i-1]]
-            groups[group_mapping[A[i]]].append(A[i])
+            #groups[group_mapping[A[i]]].append(A[i])
         else:
             group_mapping[A[i]] = A[i]
-            groups[A[i]].append(A[i])
+            #groups[A[i]].append(A[i])
     group_sum[group_mapping[A[i]]] += A[i]
 
 # 0 とM-1をマージ
 if 0 == A[0] and M-1 == A[-1]:
-    for n in groups[group_mapping[M-1]]:
-        groups[0].append(n)
+    #for n in groups[group_mapping[M-1]]:
+    #    groups[0].append(n)
     group_sum[0] += group_sum[group_mapping[M-1]]
-    groups.pop(group_mapping[M-1])
+    #groups.pop(group_mapping[M-1])
     group_sum.pop(group_mapping[M-1])
 #print(groups)
 #print(group_mapping)
