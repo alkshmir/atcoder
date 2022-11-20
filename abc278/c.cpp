f2b76ca62db8ce20d730c24a55fc61f3c686f3e6
alkshmir <shirakam.itsuk252@gmail.com>
2022-11-20 11:58:48 +0900

    abc278 c++

--- abc278/c.cpp
+++ abc278/c.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int main(){
     int64_t N, Q;
     cin >> N >> Q;
-    vector<int64_t> T(Q+1), A(Q+1), B(Q+1);
+    vector<int64_t> T(Q), A(Q), B(Q);
     for (int64_t i = 0; i<Q; i++){
         cin >> T.at(i) >> A.at(i) >> B.at(i);
     }
-    vector<std::set<int64_t>> follows(N+2);
+    std::set<std::pair<int64_t, int64_t>> follows;
 
     for (int64_t i = 0; i<Q; i++){
         if (T[i] == 1){
-            follows[A[i]-1].insert(B[i]-1);
+            follows.insert(make_pair(A[i], B[i]));
         }else if (T[i] == 2){
-            follows[A[i]-1].erase(B[i]-1);
+            follows.erase(make_pair(A[i], B[i]));
         }else if (T[i] == 3){
-            if (follows[A[i]-1].count(B[i]-1) && follows[B[i]-1].count(A[i]-1)){
+            if (follows.count(make_pair(A[i], B[i])) && follows.count(make_pair(B[i], A[i]))){
                 cout << "Yes" << endl;
             }else{
                 cout << "No" << endl;
